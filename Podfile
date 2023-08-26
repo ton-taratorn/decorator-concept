38a93ef0222f3a3f8ea134f5c59c1d4dc9e98509
ton-taratorn <taratorn.dea@ttbspark.com>
2023-08-26 18:33:32 +0700

    Add xcode project to git

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '16.0'
+
+target 'decorator-concept' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for decorator-concept
+
+end
---
+++ decorator-concept/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  decorator-concept
+//
+//  Created by Taratorn Deachboon on 26/8/2566 BE.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
